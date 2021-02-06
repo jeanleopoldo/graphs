b7d1a9530a55da85e6f917de4b971398c563c6c4
Carol <carol.mazzuco@gmail.com>
2021-02-06 16:02:03 -0300

    included file

---
+++ breadth_first_search/BFS.py
@@ -0,0 +1,25 @@
+from core.Graph import Graph
+
+class bfs:
+    def __init__(self, graph):
+        self.graph = graph
+        self.queue = []
+        self.queue.append(self.graph.get_root())
+
+    def search(self):
+        index = 0
+        while len(self.queue) != 0:
+            current_node = self.queue.pop()
+            current_node_edges = current_node.get_edges()
+            print("{} : {}".format(index, current_node.get_sequence()))
+            visited_nodes = []
+
+            index = index+1
+            nodes = self.graph.get_nodes()
+            for edge in current_node_edges:
+                for node in nodes:
+                    if node.get_sequence() == current_node.get_sequence():
+                        continue
+                    if node.get_sequence() == edge.get_end() and node.get_sequence() not in visited_nodes:
+                        self.queue.append(node)
+                        visited_nodes.append(node.get_sequence())
